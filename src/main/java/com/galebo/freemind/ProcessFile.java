177df47d3214ff854445646cbaf333df425d79a1
galebo <galebo@163.com>
2014-02-13 22:31:32 +0800

    add mm process

--- src/main/java/com/galebo/freemind/ProcessFile.java
+++ src/main/java/com/galebo/freemind/ProcessFile.java
@@ -2,17 +2,28 @@ package com.galebo.freemind;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringEscapeUtils;
 
 public class ProcessFile {
 	public static void main(String[] args) throws IOException {
-		List<String> lines = FileUtils.readLines(new File(""));
+		process("/WORK/git/nginx-study/nginx_main.mm");
+		process("/WORK/git/nginx-study/nginx_main1.mm");
+		System.out.println("exe ok");
+	}
+
+	private static void process(String fileName) throws IOException {
+		File file = new File(fileName);
+		List<String> lines = FileUtils.readLines(file);
+		List<String> out_lines = new ArrayList<String>();
 		for (Iterator<String> iterator = lines.iterator(); iterator.hasNext();) {
 			String string =  iterator.next();
-			string.replaceAll("", "");
+			out_lines.add(StringEscapeUtils.unescapeHtml(string.replaceAll("FOLDED=\"true\" ", "")));
 		}
+		FileUtils.writeLines(new File("./"+file.getName()+".txt"),out_lines);
 	}
 }
